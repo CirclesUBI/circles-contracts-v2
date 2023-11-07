e2b5a3aa0241e1c407efbcdfebb49ac334496084
Benjamin Bollen <benjamin.bollen@gnosis.io>
2023-11-07 19:46:33 +0000

    (test): improve initial test for transfer

--- test/circles/TemporalDiscount.t.sol
+++ test/circles/TemporalDiscount.t.sol
@@ -8,47 +8,46 @@ import "./MockTimeCircles.sol";
 contract TemporalDiscountTest is Test {
     
     // Constants
-    
     uint256 public constant EXA = uint256(10**18);
 
     // State variables
-
     MockTimeCircles public mockTimeCircles;
 
     address alice = makeAddr("alice");
     address bob = makeAddr("bob");
 
-    // Public functions
-
+    // Setup function
     function setUp() public {
         mockTimeCircles = new MockTimeCircles(alice);
         // set the clock one second after zero time
         setUpTime(mockTimeCircles.zeroTime() + 1);
+        
+        // Providing Alice with some tokens to start with
+        mockTimeCircles.mint(10 * EXA);
     }
 
-    function test_SimpleMint() public {
-        // mint one token for Alice
-        mockTimeCircles.mint(1 * EXA);
-        assertEq(mockTimeCircles.balanceOf(alice), EXA);
+    // Unit test to verify that the balances are updated correctly after a transfer
+    function testTransferUpdatesBalances() public {
+        // Alice's initial balance should be 10 EXA
+        assertEq(mockTimeCircles.balanceOf(alice), 10 * EXA);
+
+        // Bob's initial balance should be 0
+        assertEq(mockTimeCircles.balanceOf(bob), 0);
+
+        // Perform the transfer from Alice to Bob
+        vm.prank(alice); // Make the next call come from Alice's address
+        mockTimeCircles.transfer(bob, 2 * EXA);
+
+        // Check Alice's balance after the transfer
+        assertEq(mockTimeCircles.balanceOf(alice), 8 * EXA, "Alice's balance should have decreased by 2 EXA.");
+
+        // Check Bob's balance after the transfer
+        assertEq(mockTimeCircles.balanceOf(bob), 2 * EXA, "Bob's balance should be increased by 2 EXA.");
     }
 
-    function testFail_SimpleDiscounting() public {
-        // mint one token for Alice
-        mockTimeCircles.mint(1 * EXA);
-        assertEq(mockTimeCircles.balanceOf(alice), EXA);
-        uint256 waitOneWeekAndOneSecond =
-            mockTimeCircles.DISCOUNT_WINDOW() + 1;
-        skip(waitOneWeekAndOneSecond);
-        assertEq(mockTimeCircles.balanceOf(alice), EXA);
-    }
-
-    // function
-
-    // Private functions
-
+    // Private helper function to set up the blockchain time
     function setUpTime(uint256 _startTime) private {
-        // note: vm.wrap is not visible... ?
-        //       so abuse skip to set the time
-        skip(_startTime - block.timestamp);
+        // Move the blockchain time to _startTime
+        vm.warp(_startTime);
     }
 }
